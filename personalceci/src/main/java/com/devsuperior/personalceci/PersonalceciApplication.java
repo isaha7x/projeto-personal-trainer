5e854d45b5b2653b4ece30fa52cdd1e736e5a017
isabelasilva <isabelasilva@touchtec.com.br>
2021-09-10 00:44:03 -0300

    React e Spring

---
+++ personalceci/src/main/java/com/devsuperior/personalceci/PersonalceciApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.personalceci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalceciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalceciApplication.class, args);
+	}
+
+}
---
+++ personalceci/src/test/java/com/devsuperior/personalceci/PersonalceciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.personalceci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalceciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
