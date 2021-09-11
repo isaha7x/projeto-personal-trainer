f2c0a435e2c590340f52a9c928d62fc229b87b22
isabelasilva <isabelasilva@touchtec.com.br>
2021-09-11 00:49:37 -0300

    Adicionando componentes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,20 @@
+import NavBar from 'components/NavBar';
+import DataTable from 'components/NavBar/DataTable';
+import Footer from 'components/NavBar/Footer';
 import React from 'react';
 
 
 function App() {
   return (
+    <>
+      <NavBar />
     <div >
       <h1 className="text-primary">Meu anjo azul</h1>
+
+      <DataTable />
     </div>
+    <Footer />
+    </>
   );
 }
 
